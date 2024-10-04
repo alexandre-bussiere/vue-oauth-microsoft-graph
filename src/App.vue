4ccb1293498439f7a57644918cb91529ea88f32a
aldesse <Bussiere.alexandre@gmail.com>
2024-10-04 08:43:56 +0200

    Ex5: baseHeader / baseFooter

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <base-header />
     <HomePage />
+    <base-footer />
   </div>
 </template>
 
 <script>
 import HomePage from './pages/HomePage.vue'
+import BaseHeader from './components/base_header.vue'
+import BaseFooter from './components/base_footer.vue'
 
 export default {
   name: 'App',
   components: {
     HomePage,
+    BaseHeader,
+    BaseFooter
   },
 }
 </script>
